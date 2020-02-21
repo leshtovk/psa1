0a0d94903095989c43bc08d8dd000ffd3fac4166
Kosta Leštov <kosta.lestov@student.fmf.uni-lj.si>
2020-02-21 09:47:09 +0100

    finished sample problem, no numpy

--- poskusna.py
+++ poskusna.py
@@ -1,80 +1,86 @@
 # N -- length of the competition in minutes
 # K -- length of the interruption in minutes
 # L -- number of teams in the competition
+# X -- input matrix (list of tuples)
+# Y -- output matrix (list of lists)
 
-# get a matrix of the numerical input data
+# get a matrix of the numerical input data -- the input matrix
 def parse_input(fname):
     
     fhandle = open(fname)
-    i = 1
     X = []
+    i = 1
     
     for ln in fhandle:
         if i % 2 == 1:
-            strings = ln.split()
-            ints = []
             
-            for j in range(len(strings)):
-                ints.append(int(strings[j]))
-            X.append(ints)
+            # split the line and store the data as integers 
+            ln_data = tuple(map(int, ln.split()))            
+            X.append(ln_data)
         
         i = i + 1
         
     return X
 
 # get all the possible scores for a team
-def get_scores(eff, K):
-    # `eff` is a list of a team's minute-by-minute effectiveness 
+# `eff` is a list of a team's minute-by-minute effectiveness
+def get_scores(eff, N, K):
+     
     scores = []
     
-    for i in range(len(eff) - K + 1):
-        # `i` is the second when the interruption starts
+    for i in range(N - K + 1):
+        # `i` is the minute when the interruption starts
         score = 0
+        inactive = range(i, i+K)
 
         # add up the effectiveness from the 'active' minutes
-        for j in range(len(eff)):
-            if j not in range(i, i+K): 
+        for j in range(N):
+            if j not in inactive: 
                 score = score + eff[j]
+        
         scores.append(score) 
 
     return scores
     
-# get a matrix that contains the possible scores of the teams as rows
+# get a matrix that contains the possible scores of the teams as rows -- the output matrix
 # 
-# then the j-th column (starting at 0) is the outcome of the competition
-# in the case of the interruption starting on the j-th second
+# the i-th column (starting at 0) is the outcome of the competition
+# in the case of the interruption starting on the i-th minute
 def outcomes(fname):
 
     X = parse_input(fname)
+    (N, K, L) = (X[0][i] for i in range(3))
     Y = []
-    K = X[0][1]
     
-    for i in range(X[0][2]):
+    for i in range(L):
         # skip the first row of X, which contains N, K, L
-        Y.append(get_scores(X[i+1], K))
+        Y.append(get_scores(X[i+1], N, K))
     
-    return Y
+    # we're also returning `L` so we don't have to compute `len(Y)` in `best_placement`
+    return (Y, L)
     
 # get the best possible placement for the first team
-def best_placement(Y): 
+def best_placement(Y, L): 
     
     placements = []
     
+    # iterate over columns
     for i in range(len(Y[0])):
         column_i = []
+        score = Y[0][i]
         
-        for j in range(len(Y)):
+        for j in range(L):
             column_i.append(Y[j][i])
         
-        # sort the list in descending order to get the first index 
+        # sort the column in descending order to get the first index 
         # that matches the score of the first team
         column_i.sort(reverse = True)
-        placement = column_i.index(Y[0][i]) + 1
+        placement = column_i.index(score) + 1
         placements.append(placement)
             
     return min(placements)
     
     
 fname = input('Enter a file name: ')
-Y = outcomes(fname)
-print(best_placement(Y))
+(Y, L) = outcomes(fname)
+print(best_placement(Y, L))
