86e365436ea5468489c87b564de30587fde2159b
leshtovk <leshtov1@outlook.com>
2020-02-29 20:15:19 +0100

    sent solution for the first problem

--- teden2/naloga1.py
+++ teden2/naloga1.py
@@ -1,3 +1,4 @@
+
 (N, Z, K, S) = tuple(map(int, input().split()))
 
 narocila = [[0, 0] for i in range(N)]
@@ -5,53 +6,69 @@ for i in range(N):
     narocila[i] = tuple(map(int, input().split()))
 
 num_of_days = narocila[-1][0]
+
+# `orders` is a list of all the orders from `narocila` 
+# where the element in position i is the order for day (i+1) 
 orders = [0 for i in range(num_of_days)]
 for i in range(N):
     orders[narocila[i][0] - 1] = narocila[i][1]
-    
-def strategy(num_of_days, orders, init_inventory, prod_capacity, storage_charge):
+
+def strategy():
     """Compute the optimal strategy for filling all the orders, 
        assuming we don't sell any of the initial inventory."""
        
     order = inventory = orders[-1]
     production = 0
-    
-    debt = inventory * storage_charge
-    strat = [[0, 0] for i in range(num_of_days - 2, -1, -1)] + [[inventory, production]]
+    strat = [[0, 0, 0] for i in range(num_of_days - 2, -1, -1)] + [[inventory, production, order]]
     
     for i in range(num_of_days - 2, -1, -1):    
         order = orders[i]
-        production = min(prod_capacity, inventory)
+        production = min(K, inventory)
         inventory = inventory + order - production
-        
-        debt = debt + (inventory * storage_charge)
-        strat[i] = [inventory, production]
-        
-    inventory = init_inventory
-    if inventory < order: return (strat, -1)
-    else: return (strat, debt)
-     
-def optimal_strategy(num_of_days, orders, init_inventory, storage_charge, strategy, debt):
-    """Account for storing the initial inventory"""
-    
-    if debt == -1: print(-1)
-    else: 
-        total_orders = sum(orders)
-        if init_inventory > total_orders: init_inventory = total_orders
-        
-        for i in range(1, num_of_days): 
-            day_i_production = strategy[i-1][1]
-            if day_i_production != 0 and init_inventory != 0:
+        strat[i] = [inventory, production, order]
             
-                if day_i_production >= init_inventory:
-                    debt = debt + (init_inventory * i * storage_charge)
-                    init_inventory = 0
-                    
-                else:
-                    debt = debt + (day_i_production * i * storage_charge)
-                    init_inventory = init_inventory - day_i_production
-        
-        print(debt)
+    if Z < inventory: return -1
+    else: return strat
+     
+     
+def adjust_strategy(strategy, Z):
+    """Include the initial inventory in the strategy obtained by `strategy()`"""
 
-strategy, debt = strategy(num_of_days, orders, Z, K, S)
-optimal_strategy(num_of_days, orders, Z, S, strategy, debt)
+    if Z > sum(orders): Z = sum(orders)
+    inventory = [strategy[i][0] for i in range(num_of_days)]
+    production = [strategy[i][1] for i in range(num_of_days)]
+    
+    for i in range(num_of_days - 1):
+        if i == 0: inventory[i] = Z
+        
+        if inventory[i+1] <= inventory[i] - orders[i]:
+            production[i] = 0
+            inventory[i+1] = inventory[i] - orders[i]
+        else: 
+            production[i] = inventory[i+1] - inventory[i] + orders[i]
+        
+        if orders[i] >= Z:
+            Z = 0 
+            break
+        else: 
+            Z = Z - orders[i]
+            
+    adjusted_strategy = [[inventory[i], production[i], orders[i]] for i in range(num_of_days)]
+    return adjusted_strategy
+    
+def debt(optimal_strategy):
+    """Calculate the debt that will be owed after filling all the orders"""
+    
+    debt = 0
+    for i in range(num_of_days):
+        debt = debt + optimal_strategy[i][0] * S
+    
+    return debt
+    
+    
+strat = strategy()
+if strat == -1: print(-1)
+else: 
+    optimal = adjust_strategy(strat, Z)
+    print(debt(optimal))
+    
--- teden2/test.py
+++ teden2/test.py
@@ -1,7 +1,13 @@
-x = 0
-n = int(input())
+(N, Z, K, S) = tuple(map(int, input().split()))
 
-for i in range(n): 
-    x = x + int(input())
+narocila = [[0, 0] for i in range(N)]
+for i in range(N): 
+    narocila[i] = tuple(map(int, input().split()))
 
-print(x)
+num_of_days = narocila[-1][0]
+orders = [0 for i in range(num_of_days)]
+for i in range(N):
+    orders[narocila[i][0] - 1] = narocila[i][1]
+    
+print('No. of days:', num_of_days)
+print('Orders:', orders)
